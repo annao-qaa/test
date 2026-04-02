eef57afc71cd97d8b1ce1851729c17e4287cecf1
annao-qaa <annaostrovskaa5828@gmail.com>
2026-04-02 11:34:39 +0300

    Update ReproTest.java

--- src/test/java/example/ReproTest.java
+++ src/test/java/example/ReproTest.java
@@ -1,11 +1,13 @@
 package example;
 
 import io.qameta.allure.Allure;
+import io.qameta.allure.AllureId;
 import org.junit.jupiter.api.Test;
 
 public class ReproTest {
 
     @Test
+    @AllureId("381071")
     void testLoginSuccess() {
         Allure.step("Проверить предусловие выполнения: страница входа открыта");
         Allure.step("Выполнить действие: попытаться войти");
