a07803a0974b406ac80aa9b3ec6b41060c7f6626
annao-qaa <annaostrovskaa5828@gmail.com>
2026-04-02 17:54:40 +0300

    Update ReproTest.java

--- src/test/java/example/ReproTest.java
+++ src/test/java/example/ReproTest.java
@@ -8,13 +8,13 @@ import org.junit.jupiter.api.Test;
 public class ReproTest {
 
     @Test
-    @AllureId("381079")
+    @AllureId("381082")
     void testLoginSuccess() {
         Allure.step("Проверить предусловие выполнения: страница входа открыта");
         Allure.step("Выполнить действие: попытаться войти");
         Allure.step("Ожидаемый результат: логин успешен");
         Allure.step("Ожидаемый результат: отображается имя пользователя");
 
-        Assertions.fail("Intentional failed for repro");
+        throw new RuntimeException("Intentional broken for repro");
     }
 }
